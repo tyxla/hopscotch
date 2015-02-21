3f20bf48aa24af3660d2528ce2b51fd1c60cb50b
Marin Atanasov <tyxla@abv.bg>
2015-02-21 23:52:09 +0200

    base docs

--- src/hopscotch.js
+++ src/hopscotch.js
@@ -2,12 +2,24 @@
 // Copyright (c) 2015 Marin Atanasov (www.marinatanasov.com)
 // Hopscotch is licensed under the terms of the MIT License
 
+/**
+ * Hopscotch - Javascript library for building infinite 2D paths that user can navigate through, similar to hopscotch playgrounds.
+ *
+ * @param container {mixed} - either a dom element reference or the string id of the container
+ * @param options {object} - general options
+ */
 var Hopscotch = function(container, options) {
 	this.initialize(container, options);
 }
 
 Hopscotch.prototype = {
 
+	/**
+	 * Constructor.
+	 *
+	 * @param container {mixed} - either a dom element reference or the string id of the container
+	 * @param options {object} - general options
+	 */
 	initialize: function(container, options) {
 		var self = this;
 
