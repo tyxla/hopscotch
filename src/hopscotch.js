4e4c25480d18a4f6674e200db1e06c53bd9882aa
Marin Atanasov <tyxla@abv.bg>
2015-03-13 00:47:05 +0200

    build the playground

--- src/hopscotch.js
+++ src/hopscotch.js
@@ -17,7 +17,9 @@
 	 * Default settings.
 	 * @type {Object}
 	 */
-	var defaultSettings = {};
+	var defaultSettings = {
+		stepClass: 'hopscotch-step'
+	};
 
 	/**
 	 * Creates a new Hopscotch instance.
@@ -26,9 +28,11 @@
 	 * @param  {Object}      settings 
 	 */
 	var Hopscotch = function(container, settings) {
-		this.container = $(container);
+		this.$container = $(container);
 
 		this.settings = $.extend({}, defaultSettings, settings);
+
+		this.steps = {};
 
 		this.init();
 	}
@@ -44,7 +48,51 @@
 	 * Build the playground.
 	 */
 	Hopscotch.prototype.build = function() {
+		var _this = this;
 
+		try {
+			var maxRow = -1;
+			var maxCol = -1;
+
+			this.$container.find('.' + this.settings.stepClass).each(function() {
+				var _row = $(this).data('row');
+				var _col = $(this).data('col');
+				var row = parseInt(_row);
+				var col = parseInt(_col);
+
+				if (isNaN(row)) {
+					throw new Error('Hopscotch error: Incorrect data-row: "' + _row + '". Please, use integer values only.');
+				}
+
+				if (isNaN(col)) {
+					throw new Error('Hopscotch error: Incorrect data-col: "' + _col + '". Please, use integer values only.');
+				}
+
+				var key = row + "_" + col;
+
+				if (key in _this.steps) {
+					throw new Error('Hopscotch error: Duplicate step detected: row: ' + _row + ', col: ' + _col + '. There should be only one step with the same row and col.');
+				}
+
+				Object.defineProperty(_this.steps, key, {
+					__proto__: null,
+					value: $(this)
+				});
+
+				$(this).css('top', _row * 100 + '%');
+				$(this).css('left', _col * 100 + '%');
+
+				maxRow = Math.max(maxRow, row);
+				maxCol = Math.max(maxCol, col);
+			});
+
+			if (maxRow > -1 && maxCol > -1) {
+				this.$container.css('width', (maxCol + 1) * 100 + '%');
+				this.$container.css('height', (maxRow + 1) * 100 + '%');
+			}
+		} catch (e) {
+			console.error(e.message);
+		}
 	}
 
 	/**
@@ -53,16 +101,16 @@
 	 * @return {jQuery Object}
 	 */
 	$.fn.hopscotch = function(settings) {
-		return this.each(function() {
 
+		return this.each(function() {
 			var $container = $(this);
 			var instance = $container.data('hopscotch');
 
 			if (!instance) {
 				$container.data('hopscotch', new Hopscotch(this, settings));
 			}
-
 		});
+
 	}
 
 })(jQuery, document, window);
